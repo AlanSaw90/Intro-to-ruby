9fedd9a3576575582c2ce1c8d8e68faad7408974
AlanSaw90 <36130555+AlanSaw90@users.noreply.github.com>
2021-06-08 12:49:04 +0800

    Update ruby2.rb

--- introruby/ruby2.rb
+++ introruby/ruby2.rb
@@ -1,4 +1,12 @@
 puts 'What is you name?'
 name = gets.chomp
 
-puts "My name is #{name}."
+puts "Where was you born?"
+born = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+
+puts "My name is #{name}, I was born in #{born}.
+In year 2021, I am #{age} years old.
+Today is my first day of class at Rambutan Code Acedemy."
